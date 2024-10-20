88ae8f5a2134bdaaf47aac28d9da7a8ab3b9813b
Illia Medvid-Hnepa <illyagnepa1@gmail.com>
2024-10-20 13:08:20 +0300

    Added Tasks 1,2,3

---
+++ Task_2.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def find_min(root):
+    current = root
+    while current.left is not None:
+        current = current.left
+    return current
+
+# Тест
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+# Знаходження найменшого значення
+min_node = find_min(root)
+if min_node:
+    print(f"Найменше значення у дереві: {min_node.val}")
